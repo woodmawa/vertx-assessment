6b6b3288c4e78d2003b64aba9cfbc8a2af8f8386
woodmawa <will.woodman@btinternet.com>
2022-01-26 09:41:04 +0000

    fixed appconfig.groovy

--- src/main/groovy/ioc/TestActorDI.groovy
+++ src/main/groovy/ioc/TestActorDI.groovy
@@ -6,10 +6,11 @@ import io.micronaut.context.ApplicationContext
 import javax.inject.Inject
 
 class TestActorDI {
-    @Inject Actor actor //should inject a prototype unnamed actor
+    @Inject Actor actor //should inject a prototype unnamed actor - fails with NPE when used 
 
     ApplicationContext context = ApplicationContext.run()
 
+    //doing direct lookup generates the bean !
     def tryBeanLookup(){
         def bean = context.run().getBean(Actor)
         bean
