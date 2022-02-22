5d71957ad7285e6b6596e27fbe0b0764f5e7ca68
woodmawa <will.woodman@btinternet.com>
2022-02-22 17:11:20 +0000

    put in on failure handler when in circuit breaker execute closure

--- src/main/groovy/scripts/rxJavaScriptTryOut.groovy
+++ src/main/groovy/scripts/rxJavaScriptTryOut.groovy
@@ -1,14 +1,18 @@
 package scripts
 
 import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.ObservableEmitter
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
+import io.reactivex.rxjava3.subjects.PublishSubject
+import org.reactivestreams.Subscription
 
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicInteger
 import io.reactivex.rxjava3.functions.Predicate
 import java.util.stream.Collector
 import java.util.stream.Collectors
+import java.util.stream.IntStream
 
 println ">> observe flowable with map and filter"
 Flowable<Integer> flow = Flowable.range(1, 5)
@@ -41,10 +45,44 @@ def res = Flowable.range(1, 10)
 
 res.subscribe(System.out::println, Throwable::printStackTrace)
 
+/*  this works ok
 Flowable<Long> timedFlow = Flowable.timer(1, TimeUnit.SECONDS)
 timedFlow.blockingSubscribe({println it}, Throwable::printStackTrace)
 Iterable iter = timedFlow.blockingIterable(5)
 println "${iter.toList()}"
+*/
 
-Flowable.just(*['a','b','c']).subscribe({println it})
 
+Flowable.just(*['a','b','c']).subscribe{println "from spread list, char $it"}
+
+Flowable.fromStream (["a", "b", "c"].stream()).subscribe {println "from steam $it"}
+Flowable.fromStream (["a", "b", "c"].stream())
+        .doOnSubscribe { Subscription subscription -> subscription.request(1)}
+        .subscribe{println "with subscription $it"}
+
+def computeFunction = {println "\tcompute integer $it"}
+
+//doesnt complete to 10 before it stops, need to use blockingSubscribe to get to end of list
+Flowable.range(1, 10)
+        .observeOn(Schedulers.computation())
+        .blockingSubscribe(computeFunction)
+
+
+PublishSubject<Integer> source = PublishSubject.<Integer>create()
+source.subscribe({println "received from publishSubject  $it"})
+source.onNext(1)
+source.onNext(2)
+source.onNext(2)
+source.onComplete()
+
+source.test().assertNoValues()//test().assertEmpty()
+
+/*
+PublishSubject<Integer> source = PublishSubject.<Integer>create{ ObservableEmitter emitter -> emitter.onNext(value)}
+IntStream.range(1, 10).forEach(source::onNext)
+        source.subscribe(computeFunction, Throwable::printStackTrace)
+*/
+
+/*source.buffer(1024)
+        .observeOn(Schedulers.computation())
+        .subscribe(computeFunction, Throwable::printStackTrace)*/
