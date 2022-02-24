121cc2347b9d7d7a8183a81b4911e8ca66272e9a
woodmawa <will.woodman@btinternet.com>
2022-02-24 18:07:05 +0000

    put in on failure handler when in circuit breaker execute closure

---
+++ src/main/groovy/scripts/rxSAMproxy/MapProxy.groovy
@@ -0,0 +1,31 @@
+package scripts.rxSAMproxy
+
+import java.util.concurrent.Flow
+
+class MapProxy implements Flow.Subscriber {
+
+
+    @Override
+    void onSubscribe(Flow.Subscription subscription) {
+
+    }
+
+    @Override
+    void onNext(Object item) {
+
+    }
+
+    @Override
+    void onError(Throwable throwable) {
+
+    }
+
+    @Override
+    void onComplete() {
+
+    }
+}
+
+
+
+
