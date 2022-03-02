6b70f31020fe2a24ab6933925c85f3903d93b0ef
woodmawa <will.woodman@btinternet.com>
2022-03-02 18:37:58 +0000

    put in on failure handler when in circuit breaker execute closure

--- src/main/groovy/scripts/promisesAndFutures.groovy
+++ src/main/groovy/scripts/promisesAndFutures.groovy
@@ -1,11 +1,35 @@
 package scripts
 
+import io.micronaut.core.async.publisher.AsyncSingleResultPublisher
 import io.vertx.core.AsyncResult
+import io.vertx.core.Context
 import io.vertx.core.Promise
 import io.vertx.core.Vertx
 import io.vertx.core.Future
 
+import java.util.concurrent.Flow
+
 Vertx vertx = Vertx.vertx()
+
+Future.metaClass.then = {closure ->
+    Future future =  delegate
+
+    future.onComplete {ar ->
+        Context ctx = Vertx.vertx().getOrCreateContext()
+        def ordered = true
+        println "\treceived result [${ar.result()}]"
+        composeResult = ctx.executeBlocking({closure(ar.result()) }, ordered)  //returns new future
+    }
+
+   // Promise promise = Promise.promise()
+
+    //run the closure on another thread
+    //AsyncSingleResultPublisher arp = new AsyncSingleResultPublisher<>(closure)
+    //Flow.Subscriber sub = Promise::handle
+    //arp.subscribe(sub)
+
+
+}
 
 Future handler (Promise p) {
     def work = {
@@ -37,5 +61,21 @@ future.onComplete({AsyncResult ar ->
     println "future.onComplete(): got result : ${ar.result()}"
 })
 
+
 sleep (300)
+/*
+Future then = future.compose {String res ->
+    //compose must return a new future, with result of compose action
+    println "\tres : $res"
+
+    Promise p = Promise.promise()
+    p.complete(res.toUpperCase() )
+    p.future()
+}*/
+
+Future then = future.then {it.toUpperCase()}
+
+
+sleep (300 )
+println "then result " + then.result()
 vertx.close()
